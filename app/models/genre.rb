6ad588853eaa124cb843ead92a4c0eabcbd4ba6f
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-19 17:29:26 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,6 +11,6 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists.map {|artist_name| artist_name}
+    self.artists.map {|artist_name| artist_name.name}
   end
 end
